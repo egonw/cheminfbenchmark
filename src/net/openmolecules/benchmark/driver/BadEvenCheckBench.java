a229ea4e03e288781470e7256923d3b326369acc
Rajarshi  Guha <rajarshi.guha@gmail.com>
2009-01-11 11:13:48 -0500

    Added some more simple benchmark cases and updated the config file

---
+++ src/net/openmolecules/benchmark/driver/BadEvenCheckBench.java
@@ -0,0 +1,51 @@
+/*
+ * ChemBench - The Benchmark Suite for Cheminformatics
+ * 
+ * Copyright (c) 2009 Metamolecular, LLC (http://metamolecular.com)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package net.openmolecules.benchmark.driver;
+
+import com.sun.japex.JapexDriverBase;
+import com.sun.japex.TestCase;
+
+/**
+ * @author Rajarshi Guha
+ */
+public class BadEvenCheckBench extends JapexDriverBase {
+    private int nnum = 10000;
+    private int[] numbers;
+
+    public void initializeDriver() {
+    }
+
+    public void prepare(TestCase testCase) {
+	numbers = new int[nnum];
+	for (int i = 0; i < nnum; i++) numbers[i] = i;
+    }
+    public void run(TestCase testCase) {
+	int tally = 0;
+    
+	for (int i = 0; i < nnum; i++) {
+	    if (Math.floor((double) numbers[i] / 2.0) * 2.0 == (double) numbers[i]) tally++;
+	}
+    }
+}
