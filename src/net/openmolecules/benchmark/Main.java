fa9693ed8d3bfb3b82d931911b1472c501cf2823
Richard Apodaca <rapodaca@metamolecular.com>
2009-01-11 15:06:48 +0800

    added build, lib, src
    Signed-off-by: Egon Willighagen <egon.willighagen@gmail.com>

---
+++ src/net/openmolecules/benchmark/Main.java
@@ -0,0 +1,12 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * @author Richard L. Apodaca <rapodaca at metamolecular dot com>
+ */
+public class Main
+{
+  
+}
