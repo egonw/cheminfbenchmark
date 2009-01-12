d5e6e4964147634ba29a23c33e58cfd357c43795
Guha <rguha@156-56-93-154.dhcp-bl.indiana.edu>
2009-01-12 15:18:25 -0500

    Added config file and test class for SMILES parsing benchmark

---
+++ src/net/openmolecules/benchmark/driver/SMILESParseCDKBench.java
@@ -0,0 +1,84 @@
+/*
+ * ChemBench - The Benchmark Suite for Cheminformatics
+ * 
+ * Copyright (c) 2009 Metamolecular, LLC (http://metamolecular.com)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package net.openmolecules.benchmark.driver;
+
+import com.sun.japex.JapexDriverBase;
+import com.sun.japex.TestCase;
+import org.openscience.cdk.DefaultChemObjectBuilder;
+import org.openscience.cdk.exception.InvalidSmilesException;
+import org.openscience.cdk.smiles.SmilesParser;
+import org.openscience.cdk.smiles.smarts.parser.SMARTSParser;
+import org.openscience.cdk.smiles.smarts.parser.TokenMgrError;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *  Benchmark for SMILES parsing (test 5 in the README).
+ *
+ * Ideally, we'd like to report time per string rather than
+ * total time to parse all the SMILES. Probably soemthing to
+ * do with the finish(TestCase) method.
+ *
+ * @author Rajarshi Guha
+ */
+public class SMILESParseCDKBench extends JapexDriverBase {
+    private String[] smiles;
+    private SmilesParser smilesParser;
+    public void initializeDriver() {
+    }
+
+    public void prepare(TestCase testCase) {
+        List<String> tmp = new ArrayList<String>();
+        try {
+            String line;
+            String fileName = testCase.getParam("japex.inputFile");
+            BufferedReader in = new BufferedReader(new FileReader(fileName));
+            while ((line = in.readLine()) != null) {
+                String[] toks = line.split(" ");
+                tmp.add(toks[0]);
+             }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+
+        smiles = tmp.toArray(new String[]{});
+        smilesParser = new SmilesParser(DefaultChemObjectBuilder.getInstance());
+    }
+
+    public void run(TestCase testCase) {
+        int tally = 0;
+        for (String smile : smiles) {
+            try {
+                smilesParser.parseSmiles(smile);
+                tally++;
+            } catch (InvalidSmilesException e) {
+            }
+        }
+    }
+}
