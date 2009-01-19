24c936c00980f3831a81bd07a473506b6ed1dca5
Richard Apodaca <rapodaca@metamolecular.com>
2009-01-19 16:13:24 +0800

    added mx, mx/cdk mw/sdf comparison
    Signed-off-by: Egon Willighagen <egon.willighagen@gmail.com>

---
+++ src/net/openmolecules/benchmark/driver/CDKSDFBench.java
@@ -0,0 +1,74 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.openmolecules.benchmark.driver;
+
+import com.sun.japex.JapexDriverBase;
+import com.sun.japex.TestCase;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import org.openscience.cdk.DefaultChemObjectBuilder;
+import org.openscience.cdk.interfaces.IMolecule;
+import org.openscience.cdk.io.iterator.IteratingMDLReader;
+import org.openscience.cdk.qsar.descriptors.molecular.WeightDescriptor;
+import org.openscience.cdk.qsar.result.DoubleResult;
+
+/**
+ *
+ * @author rich
+ */
+public class CDKSDFBench extends JapexDriverBase
+{
+
+  @Override
+  public void prepare()
+  {
+  }
+
+  @Override
+  public void run(TestCase testCase)
+  {
+    IteratingMDLReader reader = getReader(testCase.getParam("japex.inputFile"));
+    WeightDescriptor calculator = new WeightDescriptor();
+    double sum = 0;
+    
+    while (reader.hasNext())
+    {
+      IMolecule molecule = (IMolecule) reader.next();
+      sum += (((DoubleResult)calculator.calculate(molecule).getValue()).doubleValue());
+    }
+
+    try
+    {
+      reader.close();
+    }
+    
+    catch (IOException e)
+    {
+      throw new RuntimeException(e);
+    }
+    
+    System.out.println("sum=" + sum);
+  }
+
+  private IteratingMDLReader getReader(String filename)
+  {
+    IteratingMDLReader result = null;
+
+    try
+    {
+      Reader raw = new FileReader(filename);
+      //BufferedReader reader = new BufferedReader(raw);
+
+      result = new IteratingMDLReader(raw, DefaultChemObjectBuilder.getInstance());
+    } catch (Exception e)
+    {
+      throw new RuntimeException(e);
+    }
+
+    return result;
+  }
+}
---
+++ src/net/openmolecules/benchmark/driver/MXSDFBench.java
@@ -0,0 +1,65 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.openmolecules.benchmark.driver;
+
+import com.metamolecular.mx.calc.MassCalculator;
+import com.metamolecular.mx.io.mdl.SDFileReader;
+import com.metamolecular.mx.model.Molecule;
+import com.sun.japex.JapexDriverBase;
+import com.sun.japex.TestCase;
+import java.io.IOException;
+
+/**
+ *
+ * @author rich
+ */
+public class MXSDFBench extends JapexDriverBase
+{
+  @Override
+  public void prepare(TestCase testCase)
+  {
+
+  }
+
+  @Override
+  public void run(TestCase testCase)
+  {
+    double sum = 0;
+    //double audit = 0;
+    MassCalculator calculator = new MassCalculator();
+    SDFileReader reader = createReader(testCase.getParam("japex.inputFile"));
+    
+    while (reader.hasNextRecord())
+    {
+      reader.nextRecord();
+
+      Molecule molecule = reader.getMolecule();
+      sum += calculator.findAveragedMass(molecule);
+      //audit += Double.parseDouble(reader.getData("PUBCHEM_MOLECULAR_WEIGHT"));
+    }
+    
+    System.out.println("sum=" + sum);
+    //System.out.println("audit=" + audit);
+    
+    reader.close();
+  }
+  
+  private SDFileReader createReader(String filename)
+  {
+    SDFileReader result = null;
+    
+    try
+    {
+      result = new SDFileReader(filename);
+    }
+    
+    catch(IOException e)
+    {
+      throw new RuntimeException(e);
+    }
+    
+    return result;
+  }
+}
